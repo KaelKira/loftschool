ab21df9dff9df6c52e8bcfd4f5a2dec58df61c70
Aleksandr <kaelkira@gmail.com>
2020-02-18 23:10:38 +0300

    Less-1-add2

--- Task6.php
+++ Task6.php
@@ -12,11 +12,22 @@ echo '<table border="1">';
 for ($tr = 1; $tr <= 10; $tr++) {
     echo '<tr>';
     for ($td = 1; $td <= 10; $td++) {
-        if(){
-            echo '<td>[' . $tr * $td . '] </td>';
+        if($td%2){
+            if($tr%2){
+                $fo = "(";
+                $fc = ")";
+            }else{
+                $fo = "";
+                $fc = "";
+            }
+        }elseif(!($tr%2)){
+            $fo = "[";
+            $fc = "]";
         }else{
-            echo '<td>(' . $tr * $td . ')</td>';
+            $fo = "";
+            $fc = "";
         }
+        echo '<td>'.$fo . $tr * $td . $fc.'</td>';
 
     }
     echo '</tr>';
--- index.php
+++ index.php
@@ -4,3 +4,5 @@ echo "<a href='Task1.php'>Task1</a>><br>";
 echo "<a href='Task2.php'>Task2</a>><br>";
 echo "<a href='Task3.php'>Task3</a>><br>";
 echo "<a href='Task4.php'>Task4</a>><br>";
+echo "<a href='Task5.php'>Task5</a>><br>";
+echo "<a href='Task6.php'>Task6</a>><br>";
