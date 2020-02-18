48e8d61b5318972cee774ef6d78aabdee9a2b493
Aleksandr <kaelkira@gmail.com>
2020-02-18 21:41:58 +0300

    Less-1-main

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<a href='Task0.php'>Task0</a>><br>";
+echo "<a href='Task1.php'>Task1</a>><br>";
+echo "<a href='Task2.php'>Task2</a>><br>";
+echo "<a href='Task3.php'>Task3</a>><br>";
+echo "<a href='Task4.php'>Task4</a>><br>";
