2754db4983c04771c39ff9d20f666b0a9580e4c2
Aleksandr Matveev <amatveev@avito.ru>
2020-02-21 21:36:13 +0300

    move-to-homework1

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-echo "<a href='Task0.php'>Task0</a>><br>";
-echo "<a href='Task1.php'>Task1</a>><br>";
-echo "<a href='Task2.php'>Task2</a>><br>";
-echo "<a href='Task3.php'>Task3</a>><br>";
-echo "<a href='Task4.php'>Task4</a>><br>";
-echo "<a href='Task5.php'>Task5</a>><br>";
-echo "<a href='Task6.php'>Task6</a>><br>";
+echo "<a href='homework1/Task0.php'>Task0</a>><br>";
+echo "<a href='homework1/Task1.php'>Task1</a>><br>";
+echo "<a href='homework1/Task2.php'>Task2</a>><br>";
+echo "<a href='homework1/Task3.php'>Task3</a>><br>";
+echo "<a href='homework1/Task4.php'>Task4</a>><br>";
+echo "<a href='homework1/Task5.php'>Task5</a>><br>";
+echo "<a href='homework1/Task6.php'>Task6</a>><br>";
