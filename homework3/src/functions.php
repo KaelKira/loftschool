9c0347aabdede00e39ea60886c81e5c34b2b70f0
Aleksandr <kaelkira@gmail.com>
2020-02-24 14:26:08 +0300

    Homework-3.1-done

--- homework3/src/functions.php
+++ homework3/src/functions.php
@@ -1,6 +1,30 @@
 <?php
 function task1()
 {
-    $file = file_get_contents('data.xml');
+    $file = file_get_contents('src/data.xml');
     $xml = new SimpleXMLElement($file);
+    echo '<b>PurchaseOrderNumber: </b>'. $xml->attributes()->PurchaseOrderNumber . '<br>';
+    echo '<b>OrderDate: </b>'. $xml->attributes()->OrderDate;
+    echo '<p>'."Адреса:" . '</p>';
+    foreach ($xml->Address as $adres){
+        echo '<p>';
+        echo '<b>Type</b>: '. $adres->attributes()->Type . '<br>';
+        echo 'Улица: '. $adres->Street->__toString() . '<br>';
+        echo 'Город: '. $adres->City->__toString() . '<br>';
+        echo 'Штат: '. $adres->State->__toString() . '<br>';
+        echo 'Индекс: '. $adres->Zip->__toString() . '<br>';
+        echo 'Страна: '. $adres->Country->__toString() . '<br>';
+        echo '</p>';
+    }
+    foreach ($xml->Items->Item as $item){
+        echo '<p>';
+        echo '<b>PartNumber</b>: '. $item->attributes()->PartNumber . '<br>';
+        echo 'Наименование товара: '. $item->ProductName->__toString() . '<br>';
+        echo 'Количество: '. $item->Quantity->__toString() . '<br>';
+        echo 'Цена: '. $item->USPrice->__toString() . '<br>';
+        echo 'Комментарий: '. $item->Comment->__toString() . '<br>';
+        echo 'Дата: '. $item->ShipDate->__toString() . '<br>';
+        echo '</p>';
+    }
+    echo '<b>Комментарии к заказу: </b>'. $xml->DeliveryNotes->__toString();
 }
