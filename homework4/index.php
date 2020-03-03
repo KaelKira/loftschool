ff73f0677ce72231e12fc5069ae5122637b9866e
Aleksandr <kaelkira@gmail.com>
2020-03-03 20:51:29 +0300

    hm4-in-progress

--- homework4/index.php
+++ homework4/index.php
@@ -1,67 +1,18 @@
 <?php
 
-interface  iCalc
+trait dopDriver
 {
-    public function checkAge($age);
-    public function multiplaer($age);
-    public function calculate($km, $time, $age);
-}
-
-abstract class Basis implements iCalc
-{
-
-    public $pricePerKm = 10;
-    public $pricePerMinet = 3;
-    public $tarifName = 'Базовый';
-
-    public function checkAge($age)
+    public function addDriverPrice()
     {
-        if ($age < 18) {
-            echo 'Возраст меньше 18 лет. Нельзя садиться за руль.';
-            return false;
-        } elseif ($age > 65) {
-            echo 'Возраст больше 65 лет. Нельзя садиться за руль.';
-            return false;
-        }
-        return true;
-    }
-
-    //повышающий коэффициент возраста
-    public function multiplaer($age){
-        if ($age >= 18 && $age <= 21) {
-            $coefficient = 1.1;
-        }else{
-            $coefficient = 1;
-        }
-        return $coefficient;
-    }
-    public function calculate($km, $time, $age)
-    {
-        if ($this->checkAge($age) === true){
-            $price = $km * $this->pricePerKm + $time * $this->pricePerMinet;
-            $price = $price * $this->multiplaer($age);
-            echo 'Тариф ' . $this->tarifName . ' (' . $km . ' км по ' . $this->pricePerKm . ' руб. плюс ' . $time . ' мин. по ' . $this->pricePerMinet . ' руб)' . ' общая стоимость: ' . $price . '<br>';
-        }else{
-            return;
-        }
+        return 100;
     }
 }
 
-class basicTarif extends Basis
+trait dopGps
 {
-
-    public $pricePerKm = 10;
-    public $pricePerMinet = 3;
-}
-
-class hourlyTarif extends Basis
-{
-    public $pricePerKm = 0;
-    public $pricePerMinet = 200;
-    public $tarifName = 'Почасовой';
-
-    public function round($time)
+    public function addGpsPrice($time)
     {
+        $pricePerHourdGps = 15;
         $hour = 0;
         if ($time > 0) {
             $hour = 1;
@@ -75,30 +26,146 @@ class hourlyTarif extends Basis
                 $flag = false;
             }
         }
-        return $hour;
-    }
-
-    public function calculate($km, $time, $age)
-    {
-        $price = $km * $this->pricePerKm + $this->round($time) * $this->pricePerMinet;
-        $price = $price * $this->multiplaer($age);
-        echo 'Тариф ' . $this->tarifName . ' (' . $km . ' км по ' . $this->pricePerKm . ' руб. плюс ' . $this->round($time) . ' часа. по ' . $this->pricePerMinet . ' руб)' . ' общая стоимость: ' . $price . '<br>';
+        $addPrice = $hour * $pricePerHourdGps;
+        return $addPrice;
     }
 }
-class StudentTarif extends Basis{
+
+interface  iCalc
+{
+    public function checkAge();
+
+    public function multiplaer();
+
+    public function calculate();
+}
+
+abstract class Basis implements iCalc
+{
+
+    public $pricePerKm = 10;
+    public $pricePerMinet = 3;
+    public $tarifName = 'Базовый';
+    public $price = 0;
+    public $km = 0;
+    public $time = 0;
+    public $age = 0;
+
+    public function __construct($km, $time, $age)
+    {
+        $this->time = $time;
+        $this->km = $km;
+        $this->age = $age;
+    }
+
+
+    public function calculate()
+    {
+        if ($this->checkAge() === true) {
+            $this->price = $this->km * $this->pricePerKm + $this->time * $this->pricePerMinet;
+            $this->price = $this->price * $this->multiplaer();
+            echo 'Тариф ' . $this->tarifName . ' (' . $this->km . ' км по ' . $this->pricePerKm . ' руб. плюс ' . $this->time . ' мин. по ' . $this->pricePerMinet . ' руб)' . ' общая стоимость: ' . $this->price . '<br>';
+        } else {
+            return;
+        }
+    }
+
+    //повышающий коэффициент возраста
+
+    public function checkAge()
+    {
+        if ($this->age < 18) {
+            echo 'Возраст меньше 18 лет. Нельзя садиться за руль.';
+            return false;
+        } elseif ($this->age > 65) {
+            echo 'Возраст больше 65 лет. Нельзя садиться за руль.';
+            return false;
+        }
+        return true;
+    }
+
+    public function multiplaer()
+    {
+        if ($this->age >= 18 && $this->age <= 21) {
+            $coefficient = 1.1;
+        } else {
+            $coefficient = 1;
+        }
+        return $coefficient;
+    }
+}
+
+class basicTarif extends Basis
+{
+
+    public $pricePerKm = 10;
+    public $pricePerMinet = 3;
+}
+
+class hourlyTarif extends Basis
+{
+    use dopDriver;
+    use dopGps;
+
+    public $pricePerKm = 0;
+    public $pricePerMinet = 200;
+    public $tarifName = 'Почасовой';
+
+    //missing parent constructor call
+    public function __construct($dopDriver, $dopGps, $km, $time, $age)
+    {
+        $this->time = $time;
+        $this->km = $km;
+        $this->age = $age;
+        if ($dopDriver) {
+            $this->price = $this->price + $this->addDriverPrice();
+        }
+        if ($dopGps) {
+            $this->price = $this->price + $this->addGpsPrice($this->time);
+        }
+    }
+
+    public function calculate()
+    {
+        $this->price = $this->price + ($this->km * $this->pricePerKm + $this->round() * $this->pricePerMinet);
+        $this->price = $this->price * $this->multiplaer();
+        echo 'Тариф ' . $this->tarifName . ' (' . $this->km . ' км по ' . $this->pricePerKm . ' руб. плюс ' . $this->round() . ' часа. по ' . $this->pricePerMinet . ' руб)' . ' общая стоимость: ' . $this->price . '<br>';
+    }
+
+    public function round()
+    {
+        $hour = 0;
+        if ($this->time > 0) {
+            $hour = 1;
+        }
+        $flag = true;
+        while ($flag == true) {
+            if (($this->time - 60) > 0) {
+                $this->time = $this->time - 60;
+                $hour++;
+            } else {
+                $flag = false;
+            }
+        }
+        return $hour;
+    }
+}
+
+class StudentTarif extends Basis
+{
     public $pricePerKm = 4;
     public $pricePerMinet = 1;
     public $tarifName = 'Студенческий';
 
-    public function checkAge($age)
+    public function checkAge()
     {
-        if ($age < 18) {
+        if ($this->age < 18) {
             echo 'Возраст меньше 18 лет. Нельзя садиться за руль.';
             return false;
-        } elseif ($age > 65) {
+        } elseif ($this->age > 65) {
             echo 'Возраст больше 65 лет. Нельзя садиться за руль.';
             return false;
-        }elseif ($age > 25){
+        } elseif ($this->age > 25) {
             echo 'Тариф Студенческий доступен только для  лиц не старше 25 лет.';
             return false;
         }
@@ -108,9 +175,9 @@ class StudentTarif extends Basis{
 }
 
 
-$tarifBasic= new basicTarif();
-$tarifBasic->calculate(1,10,40);
-$tarifHour = new hourlyTarif();
-$tarifHour->calculate(1,121,20);
-$tarifStud = new StudentTarif();
-$tarifStud->calculate(1,1,26);
+$tarifBasic= new basicTarif(1,60,40);
+$tarifBasic->calculate();
+$tarifHour = new hourlyTarif(false, false, 1, 60, 27);
+$tarifHour->calculate();
+$tarifStud = new StudentTarif(1,50,24);
+$tarifStud->calculate();
